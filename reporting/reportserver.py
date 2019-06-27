53a708fb14faccdac0c609d9c99346f00da03304
rndinfosecguy <thisisnoemail@thisdoesnotexist.com>
2019-06-27 21:25:05 +0200

    report server updare

--- reporting/reportserver.py
+++ reporting/reportserver.py
@@ -1,3 +1,19 @@
+import _thread
+import time
 import os
 
-os.system("python3 -m http.server --cgi 81")
+def start_report_server():
+	print("Starting reporting server...")
+	os.system("python3 -m http.server --cgi 81")
+
+def renew_report():
+	print("Generating report...")
+	os.system("python3 report.py")
+
+try:
+	_thread.start_new_thread(start_report_server, ())
+	while 1:
+		_thread.start_new_thread(renew_report, ())
+		time.sleep(60)
+except:
+	print("Unable to start thread.")
